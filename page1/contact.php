4feafb7dccd8c93cfc49abdd2f3c1c3105a40cc1
mackworth <mackworth@aol.com>
2024-06-27 09:57:34 -0400

    Initial Commit

---
+++ page1/contact.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta name="keywords" content="1031 exchange, closing agents, closing services, Dade title agency, deeds, documents, escrow, escrow accounts, escrow agents, escrow closings, escrow services, insurance services, International real estate, land title, Latin American real estate, lender policy, local title company,tax-deferred exchange, Miami Title Agency, Miami Title Agent, mortgages, national title company, owner policies, owner policy, owners policies, owners policy, Ponce de Leon Title, Ponce de Leon Title Services, Ponce Title, property, Qualified Intermediary attorney, real estate, real estate attorney, real estate closing, real estate records, Real Estate Title, Real Estate Title Agent , real estate transactions, recording, search records, tax liens, tax office, title, title agencies, title agency, title agent in Florida, title agent in Miami, title agents, title companies, title company, title insurance, title policies, title policy, " />
+		<meta name="title" content="Ponce de Leon Title Services" />
+		<meta name="description" content="Ponce De Leon Title Services is a lawyer-owned and operated title agency, specializing in residential, construction and commercial real estate closings for properties located in Miami-Dade and Broward County." />
+		<meta name="author" content="Alexander M, Pine Tree Software" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="robots" content="all" />
+		<link rel="apple-touch-icon" href="http://web.me.com/hmackworth/poncesite/apple-touch-icon.png" />
+		
+		<title>Contact</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="../rw_common/themes/bravo/styles.css"  />
+		<!--[if IE 6]><link rel="stylesheet" type="text/css" media="screen" href="../rw_common/themes/bravo/ie6.css"  /><![endif]-->
+		<link rel="stylesheet" type="text/css" media="screen" href="../rw_common/themes/bravo/colourtag.css"  />
+		<link rel="stylesheet" type="text/css" media="print" href="../rw_common/themes/bravo/print.css"  />
+		<link rel="stylesheet" type="text/css" media="handheld" href="../rw_common/themes/bravo/handheld.css"  />
+		<link rel="stylesheet" type="text/css" media="screen" href="../rw_common/themes/bravo/css/width/variable.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="../rw_common/themes/bravo/css/sidebar/left.css" />
+		
+		
+		<script type="text/javascript" src="../rw_common/themes/bravo/javascript.js"></script>
+		
+		<!--[if IE 6]> 
+			<style type="text/css" media="screen">body {behavior: url(../rw_common/themes/bravo/csshover.htc);}</style>
+			<script type="text/javascript" charset="utf-8">
+				var blankSrc = "../rw_common/themes/bravo/png/blank.gif";
+			</script>	
+			<style type="text/css">
+
+			img {
+				behavior:	url("../rw_common/themes/bravo/png/pngbehavior.htc");
+			}
+
+			</style>
+		<![endif]-->
+		
+		<! Copyright 2010 Alexander Mackworth. Feel free to look at the code but respect the copyright.>
+	</head>
+<body>
+<div id="topbar">
+	<img src="../rw_common/themes/bravo/images/top_bar_bg.png" alt="" style="width: 3000px; height: 16px;" />
+</div>
+<div id="topgrad">
+	<img src="../rw_common/themes/bravo/images/top_grad.png" alt="" style="width: 600px; height: 348px;" />
+</div>
+<div id="container"><!-- Start container -->
+	<div id="sidebarContainer"><!-- Start Sidebar wrapper -->
+		<div id="pageHeader"><!-- Start page header -->
+			
+			<h1>PONCE DE LEON TITLE SERVICES, INC.</h1>
+			<h2>2600 Douglas Road Suite 400<br>Coral Gables, FL  33134<br>Phone: +1.305.445.1818<br>Fax: +1.305.445.5258</h2>
+			<div class="clearer"></div>
+		</div><!-- End page header -->
+		<div id="navcontainer"><!-- Start Navigation -->
+			<ul><li><a href="../index.html" rel="self" class="currentAncestor">Home</a><ul><li><a href="../page8/page8.html" rel="self">What Is Title Insurance?</a></li><li><a href="../page4/page4.html" rel="self">Other Services</a></li><li><a href="../page7/page7.html" rel="self">Affliations</a></li><li><a href="../page6/page6.html" rel="self">What to Bring to Closing</a></li><li><a href="../page3/page3.html" rel="self">Location</a></li><li><a href="contact.php" rel="self" id="current">Contact</a></li></ul></li></ul>
+		</div><!-- End navigation -->
+		<div id="sidebar"><!-- Start sidebar content -->
+			<h1 class="sideHeader"></h1><!-- Sidebar header -->
+			<!-- sidebar content you enter in the page inspector -->
+			 <!-- sidebar content such as the blog archive links -->
+		</div><!-- End sidebar content -->
+	</div><!-- End sidebar wrapper -->
+	<div id="contentContainer"><!-- Start main content wrapper -->
+		<div id="content"><!-- Start content -->
+			
+<div class="message-text">Fill in the form below to contact us.<br /></div><br />
+
+<form action="http://poncetitle.com/page1/files/mailer.php" method="post" enctype="multipart/form-data">
+	 <div>
+
+		<label>Your Name:</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element0]" size="40"/><br /><br />
+
+		<label>Your Email:</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element1]" size="40"/><br /><br />
+
+		<label>Subject:</label> *<br />
+		<input class="form-input-field" type="text" value="" name="form[element2]" size="40"/><br /><br />
+
+		<label>Message:</label> *<br />
+		<textarea class="form-input-field" name="form[element3]" rows="8" cols="38"></textarea><br /><br />
+
+		<div style="display: none;">
+			<label>Spam Protection: Please don't fill this in:</label>
+			<textarea name="comment" rows="1" cols="1"></textarea>
+		</div>
+		<input type="hidden" name="form_token" value="30648201866748eaa8b2e8" />
+		<input class="form-input-button" type="reset" name="resetButton" value="Reset" />
+		<input class="form-input-button" type="submit" name="submitButton" value="Submit" />
+	</div>
+</form>
+
+<br />
+<div class="form-footer"></div><br />
+
+			<div class="clearer"></div>
+		</div><!-- End content -->
+		
+		<div id="footer"><!-- Start Footer -->
+			<p>&copy; 2010 Ponce de Leon Title Service, Inc.</p>
+			<div id="breadcrumbcontainer"><!-- Start the breadcrumb wrapper -->
+				
+			</div><!-- End breadcrumb -->
+		</div><!-- End Footer -->
+	</div><!-- End main content wrapper -->
+</div><!-- End container -->
+<!-- Start Google Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17715026-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script><!-- End Google Analytics -->
+</body>
+</html>
